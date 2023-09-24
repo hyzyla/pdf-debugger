faf0c15b23bf2b3e0703f2c85d7b064d19a5c7df
Yevhenii Hyzyla <hyzyla@gmail.com>
2023-09-24 13:01:51 +0200

    Make resiable

--- src/components/PDFTree.tsx
+++ src/components/PDFTree.tsx
@@ -2,19 +2,80 @@ import { PDFTreeRowDetails } from "@/components/PDFTreeDetails";
 import { PDFTreeRow } from "@/components/PDFTreeRow";
 import { PDFWalker, TreeNode } from "@/lib/pdf-walker";
 import * as core from "@hyzyla/pdfjs-core";
-import { useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 export function PDFTree(props: { pdf: core.PDFDocument; name: string | null }) {
   const [selected, setSelected] = useState<TreeNode | null>(null);
   const walker = new PDFWalker({ pdf: props.pdf });
   const root = walker.start();
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState<null | number>(null);
+
+  const startResizing = useCallback(() => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    (event: MouseEvent) => {
+      // todo: find a way to not select text when resizing
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (isResizing) {
+        const current = sidebarRef.current;
+        if (!current) return;
+
+        const resizerX = current.getBoundingClientRect().left;
+        const mouseX = event.clientX;
+
+        const widthDiff = resizerX - mouseX;
+        const sidebarWidth = sidebarRef.current?.clientWidth;
+        console.log("resizing", {
+          clientX: event.clientX,
+          left: current.getBoundingClientRect().left,
+          widthDiff,
+          sidebarWidth,
+        });
+        setSidebarWidth(sidebarWidth ? sidebarWidth + widthDiff : null);
+      }
+    },
+    [isResizing]
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
 
   return (
     <>
-      <div className="w-2/3 overflow-y-auto">
+      <div className="overflow-y-auto">
         <PDFTreeRow node={root} onClick={setSelected} />
       </div>
-      <div className="w-1/3 overflow-y-auto border-l-2 border-gray-200">
+      <div
+        className="min-w-[6px] cursor-col-resize border-l-2 border-gray-200"
+        onMouseDown={startResizing}
+      />
+      <div
+        className="overflow-y-auto"
+        ref={sidebarRef}
+        style={{ minWidth: sidebarWidth ?? "33%" }}
+      >
         {selected && <PDFTreeRowDetails node={selected} />}
       </div>
     </>
