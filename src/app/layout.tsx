57d138d071c5408a2b95c98992914e2b228fcb35
Yevhenii Hyzyla <hyzyla@gmail.com>
2023-09-27 23:26:52 +0200

    Add posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { AppPostHogProvider, PostHogPageview } from "@/app/providers";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <AppPostHogProvider>
+        <body className={inter.className}>{children}</body>
+      </AppPostHogProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { SourceViewer } from "@/components/SourceViewer";
+import Script from "next/script";
 
 export default function Home() {
-  return <SourceViewer />;
+  return (
+    <>
+      <SourceViewer />
+    </>
+  );
 }
