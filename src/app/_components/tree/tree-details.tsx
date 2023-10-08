55ecf6a105a390c38e05a91184f6de72f3c2fb54
Yevhenii Hyzyla <y.hyzyla@vchasno.ua>
2023-10-08 21:46:48 +0200

    More organization 🐝

--- src/app/_components/tree/tree-details.tsx
+++ src/app/_components/tree/tree-details.tsx
@@ -1,9 +1,7 @@
-import * as core from "@hyzyla/pdfjs-core";
-
-import { DetailProps } from "@/app/_components/tree/tree-details/_types";
 import { ArrayDetails } from "@/app/_components/tree/tree-details/array-details";
 import { BooleanDetails } from "@/app/_components/tree/tree-details/boolean-details";
 import { DictDetails } from "@/app/_components/tree/tree-details/dict-details";
+import { NameDetail } from "@/app/_components/tree/tree-details/name-details";
 import { NullDetails } from "@/app/_components/tree/tree-details/null-details";
 import { NumberDetails } from "@/app/_components/tree/tree-details/number-details";
 import { RefDetails } from "@/app/_components/tree/tree-details/ref-details";
@@ -11,28 +9,6 @@ import { StreamContentDetails } from "@/app/_components/tree/tree-details/stream
 import { StreamDetails } from "@/app/_components/tree/tree-details/stream-details";
 import { StringDetails } from "@/app/_components/tree/tree-details/string-details";
 import { TreeNode } from "@/lib/pdf-walker";
-
-function NameDetail({ node }: DetailProps<core.Name>) {
-  const syntax = node.name ? `/${node.name} /${node.obj.name}` : `[... /${node.obj.name} ...]`;
-  return (
-    <>
-      <h1>Name</h1>
-      <p>A identifier that begins with a / (e.g., /Name).</p>
-      <h3>PDF Syntax:</h3>
-      <pre>{syntax}</pre>
-      {node.index !== undefined && (
-        <>
-          <h3>Index:</h3>
-          <pre>{node.index}</pre>
-        </>
-      )}
-      <h3>Value:</h3>
-      <pre>{node.obj.name.replace(/^\//, "")}</pre>
-      <h3>Path:</h3>
-      <pre>{node.path}</pre>
-    </>
-  );
-}
 
 export function TreeNodeDetails(props: { node: TreeNode }) {
   const node = props.node;
---
+++ src/app/_components/tree/tree-details/name-details.tsx
@@ -0,0 +1,25 @@
+import * as core from "@hyzyla/pdfjs-core";
+
+import { DetailProps } from "@/app/_components/tree/tree-details/_types";
+
+export function NameDetail({ node }: DetailProps<core.Name>) {
+  const syntax = node.name ? `/${node.name} /${node.obj.name}` : `[... /${node.obj.name} ...]`;
+  return (
+    <>
+      <h1>Name</h1>
+      <p>A identifier that begins with a / (e.g., /Name).</p>
+      <h3>PDF Syntax:</h3>
+      <pre>{syntax}</pre>
+      {node.index !== undefined && (
+        <>
+          <h3>Index:</h3>
+          <pre>{node.index}</pre>
+        </>
+      )}
+      <h3>Value:</h3>
+      <pre>{node.obj.name.replace(/^\//, "")}</pre>
+      <h3>Path:</h3>
+      <pre>{node.path}</pre>
+    </>
+  );
+}
