0a15ac7e71707291df7f4ff7d03ecdf604e86c1f
Yevhenii Hyzyla <hyzyla@gmail.com>
2024-03-02 17:55:29 +0100

    Add GitHub link to footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
-import { BsFillQuestionSquareFill } from "react-icons/bs";
+import { BsFillQuestionSquareFill, BsStarFill } from "react-icons/bs";
 
 export function Footer() {
   return (
-    <Link href="about" className="flex flex-row items-center gap-x-2">
-      <BsFillQuestionSquareFill />
-      About
-    </Link>
+    <div className="flex flex-row gap-x-4">
+      <Link href="about" className="flex flex-row items-center gap-x-2">
+        <BsFillQuestionSquareFill />
+        About
+      </Link>
+      <Link
+        href="https://github.com/hyzyla/pdf-debugger"
+        className="flex flex-row items-center gap-x-2"
+      >
+        <BsStarFill />
+        Star on GitHub
+      </Link>
+    </div>
   );
 }
